24da3c63003a939417a0e82defda5c949b237d54
Connor Keogh <contact@kyoh.run>
2022-08-24 10:24:30 +1000

    Update pkg list and aliases

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,1 +1,2 @@
 alias processing=/opt/processing-4.0.1/processing
+alias lsapt="comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)"
