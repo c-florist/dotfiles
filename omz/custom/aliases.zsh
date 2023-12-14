a8b89e9a2e81cd089d7f7de9b01685a1fbad1e47
Connor Keogh <contact@kyoh.run>
2023-12-15 08:18:07 +1100

    Add golang aliases

--- omz/custom/aliases.zsh
+++ omz/custom/aliases.zsh
@@ -1,2 +1,6 @@
+# System helpers
 alias lsapt="comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)"
-alias gt="go test -v"
+
+# Golang
+alias got="go test -v"
+alias gotc="go test -v -cover"
