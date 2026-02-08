350eb5c135bdb6d98484c254ce2bdc42ca113938
Connor Flowers <cjflowers@pm.me>
2026-02-09 09:18:58 +1100

    clean up aliases

--- omz/aliases.zsh
+++ omz/aliases.zsh
@@ -1,10 +1,6 @@
 # Git
 alias gswl='git switch $(git_main_branch) && git pull'
 
-# Golang
-alias got="go test -v"
-alias gotc="go test -v -cover"
-
 # Unix
 alias lsapt="comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u)"
 alias ls="ls -A -F --color=always"
@@ -17,7 +13,6 @@ alias nosleep='caffeinate -d'
 alias j="just"
 alias tf="terraform"
 alias cat="bat -p --no-paging --theme=gruvbox-dark"
-alias lzg="lazygit"
 alias mr="mise run"
 alias k="kubectl"
 
