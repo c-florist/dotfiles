7014699fc9e18a560aea826eb67bd5161e43cbc5
Connor Flowers <cjflowers@pm.me>
2026-02-09 09:19:31 +1100

    port bash functions to zsh

--- omz/functions.zsh
+++ omz/functions.zsh
@@ -1,20 +1,31 @@
-source-search(){
-    find . \( \
-        -name node_modules -o \
-        -name .mypy_cache -o \
-        -name .ruff_cache -o \
-        -name .pytest_cache -o \
-        -name .direnv -o \
-        -name venv -o \
-        -name .venv -o \
-        -name .idea -o \
-        -name .terraform -o \
-        -name .git -o \
-        -name .aws-sam -o \
-        -name htmlcov -o \
-        -name build -o \
-        -name dist \
-    \) -prune -o -type f -exec grep -I -i "$1" {} \+
+# Make a directory and cd into it
+mkcd() {
+    mkdir -p "$1" && cd "$1"
 }
 
-ql () { qlmanage -p "$*" >& /dev/null & }
+# Show what's listening on a given port
+port() {
+    lsof -i :"$1"
+}
+
+# Extract common archive formats
+extract() {
+    if [ -f "$1" ]; then
+        case "$1" in
+            *.tar.bz2)   tar xjf "$1"     ;;
+            *.tar.gz)    tar xzf "$1"     ;;
+            *.bz2)       bunzip2 "$1"     ;;
+            *.rar)       unrar e "$1"     ;;
+            *.gz)        gunzip "$1"      ;;
+            *.tar)       tar xf "$1"      ;;
+            *.tbz2)      tar xjf "$1"     ;;
+            *.tgz)       tar xzf "$1"     ;;
+            *.zip)       unzip "$1"       ;;
+            *.Z)         uncompress "$1"  ;;
+            *.7z)        7z x "$1"        ;;
+            *)           echo "'$1' cannot be extracted via extract()" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
